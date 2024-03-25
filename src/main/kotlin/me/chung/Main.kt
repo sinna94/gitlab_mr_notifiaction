8812b481e76085d30edd3c0a80fd5939d6424e10
chunghyun <chunghyun.lee@kstec.co.kr>
2024-03-25 09:13:09 +0900

    기본 메시지 벤더를 슬랙으로 설정

--- src/main/kotlin/me/chung/Main.kt
+++ src/main/kotlin/me/chung/Main.kt
@@ -2,15 +2,12 @@ package me.chung
 
 import me.chung.gitlab.GitLabMergeRequestFetcher
 import me.chung.notification.NotificationService
-import me.chung.notification.messagesender.EmojiGenerator
 import me.chung.notification.messagesender.MessageSenderFactory
 import me.chung.notification.messagesender.MessageSenderVendor
-import me.chung.notification.messagesender.slack.LayoutBlockBuilder
-import me.chung.notification.messagesender.slack.SlackMessageSender
 import me.chung.utils.SystemEnvironmentVariableProvider
 
 fun main() {
-    val messageSenderVendor = SystemEnvironmentVariableProvider.getEnvVariable("MESSAGE_SENDER_VENDOR")
+    val messageSenderVendor = SystemEnvironmentVariableProvider.getEnvVariableOrNull("MESSAGE_SENDER_VENDOR") ?: "SLACK"
     val messageSender = MessageSenderFactory.createMessageSender(MessageSenderVendor.fromString(messageSenderVendor))
 
     NotificationService(
